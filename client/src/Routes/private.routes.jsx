d2b0a315db7a77afd6e74b46ee047e5491b3c8ee
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-09-09 21:04:07 +0530

    ok

--- client/src/Routes/private.routes.jsx
+++ client/src/Routes/private.routes.jsx
@@ -1,6 +1,7 @@
 import { Route } from "lucide-react";
 import {Navigate, Outlet} from 'react-router-dom'
 export const PrivateRoute = ({isAuthenticated = false,isAdmin = false,children})=>{
-    // if(isAuthenticated) return <Navigate to={"/"} />
+    if(isAuthenticated) return <Navigate to={"/"} />
+    if(isAdmin) return <Navigate to={"/"} />
     return children;
 }
